79e9f31e863615a0714a0d2c923332ba65480038
Theodorus Karli <90240274+TheodorusKarli@users.noreply.github.com>
2021-11-04 12:09:28 +0800

    Add files via upload

---
+++ tipe36.java
@@ -0,0 +1,30 @@
+//Theodorus Karli
+//51019020
+
+class tipe36 extends PatokInput {
+	private int harga;
+	private int kembalian;
+	
+	public void setHarga() {
+		
+		if (blok == 'A'){
+			harga=500000;
+		} else 
+		if (blok == 'B'){
+			harga=300000;
+		}
+		
+	}
+	
+	public int getHarga(){
+		return harga;
+	}
+	
+	public void setKembalian(int bayar) {
+		kembalian=bayar-harga;
+	}
+	
+	public int getKembalian() {
+		return kembalian;
+	}
+}
